583f2a292901beaae2597b4654c258133710a199
serkan göktaş <aserkangoktas@gmail.com>
2019-09-24 22:23:39 +0300

    sagda buyuk etkinlik

--- app/src/greet/greet.component.js
+++ app/src/greet/greet.component.js
@@ -5,7 +5,31 @@ class GreetController{
 
     constructor(){
         this.greeting = "Hello my dear folks!";
+        this.acts = [];
+
+        this.selact = {
+            img : "lasf",
+            isim : "Not selected",
+            aciklama : "nOt selected",
+            tarih : "0/0/0"
+        }
+
+        for(let i = 0; i<10; i++){
+            this.acts.push({
+                isim : "Zengin olma etkinliği",
+                tarih : "66/666/6666",
+                img : "teo.jpg",
+                aciklama : "Etkinlik açıklaması",
+                ayrinti : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla pellentesque dignissim enim sit amet venenatis urna cursus eget. Venenatis tellus in metus vulputate. Aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod. Sed odio morbi quis commodo odio aenean. Mauris augue neque gravida in fermentum et sollicitudin ac. Vel orci porta non pulvinar. Dictum at tempor commodo ullamcorper a lacus vestibulum. Orci ac auctor augue mauris. Egestas fringilla phasellus faucibus scelerisque eleifend. Mauris a diam maecenas sed enim ut sem. Et malesuada fames ac turpis egestas. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Id diam vel quam elementum pulvinar etiam non quam."
+            });
+        }
+
+
+        this.broaden = act => {
+            this.selact = act;
+        }
     }
+
 }
 
 export default {
--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,21 @@ module.exports = {
             { // For css
                 test : /\.css$/,
                 use : ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,  
+                use: [{
+                    loader: 'url-loader',
+                    options: { 
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    } 
+                }]
             }]
     },
 
 }
+
+
+
+
